bb16454ea4f22629f4862d6754fa766e9976f6db
Addy Williams <awillia5@gmail.com>
2025-12-08 12:44:26 -0800

    removed passport references

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,7 +7,6 @@ const {
 } = require("../controllers/userController");
 
 const { authMiddleware,  adminOnly, signToken} = require("../middleware/auth");
-const passport = require('passport');
 
 // Router
 const userRouter = express.Router();
@@ -35,26 +34,9 @@ userRouter.post("/login", loginUser);
 
 // Route to start the OAuth flow
 // When a user visits this URL, they will be redirected to GitHub to log in.
-userRouter.get(
-  '/auth/github',
-  passport.authenticate('github', { scope: ['user:email'] }) // Request email scope
-);
 
 
 // The callback route that GitHub will redirect to after the user approves.
-userRouter.get(
-  '/auth/github/callback',
-  passport.authenticate('github', {
-    failureRedirect: '/login', // Where to redirect if user denies
-    session: false // We are using tokens, not sessions
-  }),
-  (req, res) => {
-    // At this point, `req.user` is the user profile returned from the verify callback.
-    // We can now issue our own JWT to the user.
-    const token = signToken(req.user);
-    // Redirect the user to the frontend with the token, or send it in the response
-    res.redirect(`http://localhost:5173?token=${token}`);
-  }
-);
+
 
 module.exports = userRouter;
--- server.js
+++ server.js
@@ -21,7 +21,6 @@ const app = express();
 app.use(morgan('dev')); // logger
 app.use(express.json()); // body parser
 app.use(cors({origin: "http://localhost:5173"}));
-require('./config/passport');
 
 // ========= Routes ======================
 app.use('/api/users', require('./routes/userRoutes'));
